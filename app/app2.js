31fb3ffbc6e7928f3bdc56267ae675d7075626cb
lastjune <follow_leaves@foxmail.com>
2015-01-30 22:06:06 +0800

    app2.js

---
+++ app/app2.js
@@ -0,0 +1,59 @@
+var parseString=require('xml2js').parseString;
+debugger;
+var fs = require('fs');
+var json;
+var mapping={
+	"calculations":"div",
+	"calculator":"div"
+}
+fs.readFile('target.xml',function(err,data){
+    var xml = data.toString();
+	parseString(xml,function(err,result){
+	//	console.dir(JSON.stringify(result));
+		json=result;
+		json=jsonhandler(json);
+		fs.writeFile('cook.vim',JSON.stringify(json));
+	});
+});
+function jsonhandler(o){
+	var tag="div";//default treated as div element
+	if(arguments[1]){
+		tag=arguments[1];
+	}
+	var beginTag="<"+tag+">";
+	var closeTag="</"+tag+">";
+	var h=h+beginTag;
+	if(o instanceof Array){
+		for(var a in o){
+			h+=jsonhandler(o[a],mapping[a]);
+		}
+	}
+	else if(o instanceof Object){
+		for(var el in o){
+			if(o[el] instanceof Array){
+				for(var a in o[el]){
+					h+=jsonhandler(o[el][a],mapping[a]);
+				}
+			}
+			else if(o[el] instanceof Object ){//second level
+				h+=jsonhandler(o[el],mapping[el]);
+			}
+			else{
+				var type=typeof(el);
+				if(type=="string"){
+				
+				}
+				h+=el+":"+o[el];
+			}
+		}
+	}
+	h=h+closeTag;
+	return h;
+};
+
+function addProptites(s){
+	//TODO
+	if(typeof(s)=="string"){
+		s. 
+	}
+}
